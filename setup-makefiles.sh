b27cc5477c078e8e5fcf711db7b832a1c792d3b9
spkal01 <kalligeross@gmail.com>
2021-01-04 13:21:33 +0000

    Octavi bringup
    Signed-off-by: spkal01 <kalligeross@gmail.com>

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -15,17 +15,18 @@ VENDOR=xiaomi
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
-ANDROID_ROOT="${MY_DIR}/../../.."
+OCTAVI_ROOT="${MY_DIR}/../../.."
 
-HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
+HELPER="${OCTAVI_ROOT}/vendor/octavi/build/tools/extract_utils.sh"
+
 if [ ! -f "${HELPER}" ]; then
     echo "Unable to find helper script at ${HELPER}"
     exit 1
 fi
 source "${HELPER}"
 
-# Initialize the helper
-setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}"
+# Initialize the helper for common
+setup_vendor "${DEVICE}" "${VENDOR}" "${OCTAVI_ROOT}" false
 
 # Warning headers and guards
 write_headers
